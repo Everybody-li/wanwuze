3d818c3ab824fa2e9d9f29fedf80c2cc719e2be9
litaohong <1026980510@qq.com>
2025-06-18 17:06:11 +0800

    定价服务专员-型号定价-查看最大日期调整

--- wanwuze/aprc/web/servicefee/mn/getMnMaxEndDate_1_0_1.sql
+++ wanwuze/aprc/web/servicefee/mn/getMnMaxEndDate_1_0_1.sql
@@ -7,13 +7,13 @@
 
 -- ##input modelGuid string[36] NOTNULL;型号guid，必填
 -- ##input curUserId string[36] NOTNULL;登录用户id，必填
--- ##input size int[>=0] NOTNULL;每页多少行数据（默认20），必填
--- ##input page int[>=0] NOTNULL;第几页（默认1），必填
 
-select ifnull((
-select 
-left(max(DATE_ADD(t.end_date,INTERVAL 1 day)),10)
-from
-coz_category_service_fee_mn t
-where 
-model_guid='{modelGuid}'),left(now(),10)) as maxEndDate
+
+# select ifnull((
+# select
+# left(max(DATE_ADD(t.end_date,INTERVAL 1 day)),10)
+# from
+# coz_category_service_fee_mn t
+# where
+# model_guid='{modelGuid}'),left(now(),10)) as maxEndDate;
+select left(now(),10) as maxEndDate
