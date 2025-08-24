9fc9184be12687236c24bc0b8cc4f78005ba9cf1
litaohong <1026980510@qq.com>
2025-08-24 22:48:36 +0800

    取服务费-按品类-修复bug

--- wanwuze/aprc/java/servicefee/getCatServiceFees_1_0_1.sql
+++ wanwuze/aprc/java/servicefee/getCatServiceFees_1_0_1.sql
@@ -12,11 +12,12 @@
 select 
 t.category_guid as categoryGuid
 ,t.charge_type as chargeType
-,round(t.charge_value / 100 ,2) as chargeValue
+    -- 按比例除以100，按金额乘以100
+, if(charge_type = 1, round(t.charge_value / 100, 2), t.charge_value * 100) as chargeValue
 ,t.target_object as targetObject
 ,t.create_time as createTimeStr
 from
 coz_category_service_fee_log t
-# where
-# category_guid='{categoryGuid}'and del_flag='0'
+where
+category_guid='{categoryGuid}'and del_flag='0'
 order by id desc
